e1cedfa1caef24598f7bea21f8ee7a74e8f11e7b
Daniel Aragon <daniel.aragon.bermudez@gmail.com>
2018-03-01 19:55:56 -0600

    fixed docstrings

--- table.py
+++ table.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 # pylint: disable=E1120
-import io
+import ast
+import functools
 import json
 import math
 import operator
-import functools
-import operator
-from typing import Dict, List
-import ast
+from typing import Dict, List, TextIO, Optional
 
 import click
 
@@ -26,9 +24,9 @@ PERFORMANCE = (
 
 #: Demo data set 2
 TIME = (
-    "Time of excecution in seconds",
+    "Time of execution in seconds",
     ["Computer", "mafft", "mrbayes", "build-mplayer", "build-php",
-        "compress-gzip", "dcraw", "encode-flac", "gnupg"],
+     "compress-gzip", "dcraw", "encode-flac", "gnupg"],
     {
         "A": [18.95, 42.51, 163.14, 87.3, 22.06, 109.64, 13.86, 14.79],
         "B": [20.81, 49.69, 287.17, 461.28, 19.47, 92.81, 10.68, 28.27],
@@ -40,7 +38,43 @@ TIME = (
 
 
 class BenchmarkTable:
-    def __init__(self, demo: str, input_file: str, new: bool):
+    """BenchmarkTable
+
+    Wraps a data set composed of benchmarking scores.
+
+    Attributes:
+        title:
+            The title of the table.
+        headers:
+            The column headers of the table.
+        data:
+            The benchmarking data.
+        type:
+            The data set type, either LIB or HIB.
+
+    """
+
+    def __init__(self,
+                 demo: Optional[str] = None,
+                 input_file: Optional[str] = None,
+                 new: Optional[str] = None):
+        """BenchmarkTable
+
+        Wraps a data set composed of benchmarking scores.
+
+        Args:
+            demo:
+                Defaults to None. Used for selecting a demo data set should be
+                either "p" or "t".
+            input_file:
+                Defaults to None. File name of an input file that contains a
+                data set.
+            new:
+                Used to specify if the user wants to create a new table,
+                this parameter would then be this new data set's title and
+                initialize the BenchmarkTable using this data set.
+        """
+        data_set = tuple()
         if demo:
             if demo == "t":
                 data_set = TIME
@@ -63,13 +97,16 @@ class BenchmarkTable:
         self.title, self.headers, self.data, self.type = data_set
 
     def print_table(self, items: Dict[str, List[float]],
-                    output: io.IOBase = None):
+                    output: Optional[TextIO] = None):
         """Prints the data set as a markdown table.
 
         Args:
-            items: data set that should be converted to a markdown table.
+            items:
+                The data set that should be converted to a markdown table.
+            output:
+                Optional output file.
         """
-        # Set output
+        # Set output to either stdout or the output file if there is one
         writer = compose(output.write, lambda s: s + "\n") if output else print
 
         writer(f"| {str.join(' | ', self.headers)} |")
@@ -82,11 +119,11 @@ class BenchmarkTable:
         given machine
 
         Args:
-            ref_machine: the machine we use to normalize the :data:`self.data`
+            ref_machine: the machine we use to normalize the `self.data`
 
         Returns:
             A dictionary that maps computers to the list of their normalized
-            results with respect to :paramref:`ref_machine`
+            results with respect to `ref_machine`
         """
         result = {}
         for k, v in self.data.items():
@@ -103,10 +140,16 @@ class BenchmarkTable:
             result[k] = normalized
         return result
 
-    def print_markdown(self, output: io.IOBase = None):
-        """Prints markdown tables with that compare all the machines relative to each
-        other."""
-        # Set output
+    def print_markdown(self, output: Optional[TextIO] = None):
+        """Prints markdown tables with that compare all the machines
+        relative to each other.
+
+        Args:
+            output:
+                Optional output file.
+
+        """
+        # Set output to either stdout or the output file if there is one
         writer = compose(output.write, lambda s: s + "\n") if output else print
 
         # Document title
@@ -142,17 +185,19 @@ class BenchmarkTable:
                 if computer != ref_machine:
                     adj = "fast" if self.type == "LIB" else "powerful"
                     result = (
-                        f"- Computer {computer} is {mean:.2f} times" +
-                        f" as {adj} as computer {ref_machine}."
+                            f"- Computer {computer} is {mean:.2f} times" +
+                            f" as {adj} as computer {ref_machine}."
                     )
                     results.append(result)
             writer(str.join('\n', results))
 
     @staticmethod
-    def new_data_set(title: str = None) -> tuple:
+    def new_data_set(title: Optional[str] = None) -> tuple:
         """Creates a usable data set from user input
-            title (str, optional): Defaults to None. The data set's
-                                   description.
+
+        Args:
+            title:
+                Defaults to None. The data set's description.
 
         Returns:
             A data set tuple (title, headers, data, data_type).
@@ -173,14 +218,15 @@ class BenchmarkTable:
                 float(input(f"Machine {letter}'s result in test {test}: "))
                 for test in headers[1:]]
         data_type = input("Is your data LIB or HIB?: ")
-        return (title, headers, data, data_type)
+        return title, headers, data, data_type
 
     @staticmethod
     def validate_data_set(data_set):
         """Validates a given data set.
 
         Args:
-            data: a data set of the form (title, headers, data, data_type).
+            data_set:
+                A data set of the form (title, headers, data, data_type).
         """
         _, headers, data, data_type = data_set
         first = list(data.keys())[0]
@@ -196,6 +242,7 @@ class BenchmarkTable:
 def compose(*functions):
     def compose2(f, g):
         return lambda x: f(g(x))
+
     return functools.reduce(compose2, functions, lambda x: x)
 
 
@@ -208,18 +255,22 @@ def geo_mean(data: List[float]):
     """Gets the geometric mean of a list of floats.
 
     Args:
-        data: the list of floats.
+        data:
+            the list of floats.
     """
-    return math.pow(functools.reduce(operator.mul, data), 1/len(data))
+    return math.pow(functools.reduce(operator.mul, data), 1 / len(data))
 
 
 def valid_file(ctx, param, value):
     """Callback function for the "--load" option.
 
     Args:
-        ctx:   the application's context.
-        param: "--load".
-        value: the value assigned to this option.
+        ctx:
+            the application's context.
+        param:
+            "--load".
+        value:
+            the value assigned to this option.
     """
     if value is not None and not value.endswith((".py", ".json", ".JSON")):
         raise click.BadParameter('Should be a .py or .json file')
@@ -230,9 +281,12 @@ def valid_demo(ctx, param, value):
     """Callback function for the "--demo" option.
 
     Args:
-        ctx:   the application's context.
-        param: "--demo".
-        value: the value assigned to this option.
+        ctx:
+            the application's context.
+        param:
+            "--demo".
+        value:
+            the value assigned to this option.
     """
     if value is not None and value not in ["p", "t"]:
         raise click.BadParameter('Should be either "t" for the time demo,' +
@@ -249,19 +303,24 @@ def valid_demo(ctx, param, value):
               help='creates a new data set with the provided title')
 @click.option('--output', default=None,
               help='saves output to the specified file')
-def cli(demo: str, load: str, new: bool, output: str):
+def cli(demo: Optional[str], load: Optional[str],
+        new: Optional[str], output: Optional[str]):
     """Click application
 
     Args:
-        demo:   What the user passed with the --demo flag. Used for selecting
-                a demo accepts "p" or "t".
-        load:   What the user passed with the --load flag. Used to specify if
-                a python or json file should be loaded.
-        new:    What the user passed with the --new flag. Used to specify if
-                the user wants to a new table, this parameter would then be
-                this new data set's title.
-        output: What the user passed with the --output flag. Used to specify
-                if and to which file the output should be saved.
+        demo:
+            What the user passed with the --demo flag. Used for selecting a
+            demo accepts "p" or "t".
+        load:
+            What the user passed with the --load flag. Used to specify if a
+            python or json file should be loaded.
+        new:
+            What the user passed with the --new flag. Used to specify if the
+            user wants to a new table, this parameter would then be this new
+            data set's title.
+        output:
+            What the user passed with the --output flag. Used to specify if
+            and to which file the output should be saved.
     """
     benchmark = BenchmarkTable(demo, load, new)
     if output:
